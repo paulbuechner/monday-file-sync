daccd9db510cc06d3f0effe4f4f902548b967e9c
Paul Büchner <paul-buechner@outlook.de>
2021-07-23 00:07:09 +0200

    feat: add `constants.ts`

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const __prod__ = process.env.NODE_ENV === "production";
+
+// Sessions
+export const { CORS_ORIGIN } = process.env;
+
+// monday
+export const { MONDAY_API_KEY } = process.env;
