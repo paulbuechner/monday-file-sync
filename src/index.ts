20778eb1e2999c8ee7f60baad8ddd4c35252d8bd
paulbuechner <paul-buechner@outlook.de>
2021-07-24 22:10:51 +0200

    feat: listen to file changes via `chokidar`

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,18 @@
-/* eslint-disable no-console */
 import "dotenv-safe/config";
-import mondaySdk from "monday-sdk-js";
-import nodemon from "nodemon";
+import chokidar from "chokidar";
 
-import config from "../nodemon.json";
-import { MONDAY_API_KEY } from "./utils/constants";
-import { upload } from "./utils/upload";
+import { logging } from "./utils/log";
 
-const monday = mondaySdk();
-monday.setToken(MONDAY_API_KEY!);
+// Initialize watcher.
+const watcher = chokidar.watch("src/sfi", {
+  ignored: /(^|[/\\])\../u, // ignore dotfiles
+  persistent: true,
+});
 
-async function logging() {
-  await monday.api(`query { users { name } }`).then((res) => {
-    console.log(res.data.users);
-    /* { data: { users: [{id: 12312, name: "Bart Simpson"}, {id: 423423, name: "Homer Simpson"}] } } */
-  });
+// One-liner for current directory
+watcher.on("change", (event, path) => {
+  console.log(event, path);
 
-  await monday
-    .api(
-      `query { boards(ids: 1507965305) { id name columns { id title type } } }`
-    )
-    .then((res) => {
-      console.log(res.data);
-      console.log(res.data.boards[0]);
-    });
-}
-
-nodemon(config);
-
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-nodemon.on("restart", async (files) => {
-  // process files
-  console.log(files);
-
-  await logging();
-  await upload();
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  logging();
 });
