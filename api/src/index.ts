5adb508c073a3d88edf389521503f2b15760efea
paulbuechner <paul-buechner@outlook.de>
2021-08-30 12:43:55 +0200

    feat(api): add scheduler

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,44 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import "dotenv-safe/config";
-import chokidar from "chokidar";
+import { watch } from "chokidar";
+import schedule from "node-schedule";
 
-import { logging } from "./resolvers/log";
-import { upload } from "./resolvers/upload";
+// import { WATCH_PATH } from "./constants";
+import { handleUpload } from "./utils/handleUpload";
+import { logger } from "./utils/logger";
 
 // Initialize watcher.
-const watcher = chokidar.watch("__tests__", {
-  ignored: /(^|[/\\])\../u, // ignore dotfiles
-  persistent: true,
-});
+const watcher = watch(
+  // absolute or relative path WATCH_PATH!
+  "__tests__",
+  {
+    ignored: /(^|[/\\])\../u, // ignore dotfiles
+    persistent: true,
+  }
+);
 
 // One-liner for current directory
-watcher.on("change", (event, path) => {
-  console.log(event, path);
+watcher.on("change", (event, _) => {
+  console.info(event);
+  console.log("Start batching...");
 
-  // Query some information
-  logging();
+  setTimeout(() => {
+    logger.change(event);
+  }, 100);
+});
 
-  // Upload file to monday
-  upload(event);
+/*  *    *    *    *    *    *
+ *  ┬    ┬    ┬    ┬    ┬    ┬
+ *  │    │    │    │    │    │
+ *  │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+ *  │    │    │    │    └───── month (1 - 12)
+ *  │    │    │    └────────── day of month (1 - 31)
+ *  │    │    └─────────────── hour (0 - 23)
+ *  │    └──────────────────── minute (0 - 59)
+ *  └───────────────────────── second (0 - 59, OPTIONAL)
+ */
+schedule.scheduleJob("10 * * * * *", () => {
+  console.log("The answer to life, the universe, and everything!");
+
+  handleUpload();
 });
