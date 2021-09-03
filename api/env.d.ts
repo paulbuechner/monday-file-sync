7b9e14b9476f1b166436e16cee96ffb6f52050b1
paulbuechner <paul-buechner@outlook.de>
2021-09-03 11:51:25 +0200

    chore(api): update types

--- api/env.d.ts
+++ api/env.d.ts
@@ -1,7 +1,14 @@
 declare namespace NodeJS {
   type ProcessEnv = {
-    NODE_ENV: "development" | "production";
-    WATCH_PATH: string;
-    MONDAY_API_KEY: string;
+    NODE_ENV: string;
+    DEV_MONDAY_API_KEY: string;
+    DEV_WATCH_PATH: string;
+    DEV_WATCH_FILE: string;
+    DEV_NOTIFICATION_IDS: string;
+    DEV_BOARD_CARRIER_ID: string;
+    PROD_MONDAY_API_KEY: string;
+    PROD_WATCH_PATH: string;
+    PROD_NOTIFICATION_IDS: string;
+    PROD_BOARD_CARRIER_ID: string;
   };
 }
