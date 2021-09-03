97c45819d30c9d4ceb75f0a7df97a7ad3f87fd62
paulbuechner <paul-buechner@outlook.de>
2021-09-03 11:58:52 +0200

    refactor(api): performance optimizations

--- api/src/utils/getUploadDir.ts
+++ api/src/utils/getUploadDir.ts
@@ -1,39 +1,58 @@
-import { getAllBoards, getItems } from "../queries/board";
+// import { DEV_WATCH_FILE } from "../constants";
+import type { BoardsProps } from "../queries/board";
+import { getItems } from "../queries/board";
 import type { FilteredItemsProps, FilterProps } from "./filter";
 import { filterItems, filterPath } from "./filter";
+import { logger } from "./logger";
 
-type UploadDirProps = {
+export type UploadDirProps = {
   filteredItems: FilteredItemsProps;
   filteredPath: FilterProps;
+  boardId: string;
 };
 
 export async function getUploadDir(
-  file: string
+  file: string,
+  boards: BoardsProps[]
 ): Promise<UploadDirProps | undefined> {
   // get dir, filename, fileextension from path
   const filteredPath = filterPath(file);
 
-  // get all boards
-  const boards = await getAllBoards();
-  // console.log(boards);
-
-  // when filteredPath OR boards is undefined -> throw ERROR
-  if (!boards || !filteredPath) {
-    console.error(
-      "Oh no! 😕 Something went wrong: Either filtered-path or boards is undefined"
+  // when filteredPath is undefined -> throw ERROR
+  if (!filteredPath) {
+    logger.error(
+      `⛔ Oh no! Something went wrong: Filtered path is undefined. <br> • ${file.replace(
+        /\\/gu,
+        "/"
+      )} <br><br> Please Update the Board manually.`
     );
 
     return;
   }
 
   // get boardId for filtered path
-  const boardId = boards.filter((b) => b.name.includes(filteredPath.dir));
+  const boardId = boards.filter((b) => {
+    if (b.description?.includes(filteredPath.dir)) {
+      return b;
+    }
+  });
   // console.log("Board-ID:", boardId);
 
-  // when boardId is undefined OR len(boardId) > 1 -> throw ERROR
-  if (!boardId || boardId.length > 1) {
-    console.error(
-      "Oh no! 😕 Something went wrong: Couldn't find specific boardId for given filter"
+  if (boardId.length === 0) {
+    logger.error(
+      `⛔ Oh no! Something went wrong: Couldn't find boardId. Did you forgot to include the Search Parameter in the boards description? <br> • ${filteredPath.filename} <br><br> Please Update the Board manually.`
+    );
+
+    return;
+  }
+
+  if (boardId.length > 1) {
+    logger.error(
+      `⛔ Oh no! Something went wrong: Couldn't find boardId. <br><br> Descriptions Search Parameter detected in multiple boards. <br> ${[
+        boardId,
+      ]} <br><br> • ${
+        filteredPath.filename
+      } <br><br> Please Update the Board manually.`
     );
 
     return;
@@ -43,10 +62,11 @@ export async function getUploadDir(
   const board = await getItems(boardId[0].id);
   // console.log(board);
 
-  // when no board for given boardId is found -> throw ERROR
+  // check if items for board returned
   if (!board) {
-    console.error(
-      "Oh no! 😕 Something went wrong: Couldn't find board for given boardId"
+    logger.error(
+      `⛔ Oh no! Something went wrong: Couldn't find board for given boardId. <br> • ${filteredPath.filename} <br><br> Please Update the Board manually.`,
+      { additional_info: boardId[0].id }
     );
 
     return;
@@ -54,8 +74,9 @@ export async function getUploadDir(
 
   // check for duplicate named groups
   if (new Set(board.groups.map((g) => g.title)).size !== board.groups.length) {
-    console.error(
-      "Oh no! 😕 Something went wrong: Check groups naming convention. Identical named groups detected."
+    logger.error(
+      `⛔ Oh no! Something went wrong: Check group names. Identical named groups detected. <br> • ${filteredPath.filename} <br><br> Please Update the Board manually.`,
+      { additional_info: boardId[0].id }
     );
 
     return;
@@ -64,5 +85,15 @@ export async function getUploadDir(
   // get the itemId including previous files with the given filename to upload to
   const filteredItems = filterItems(board.items, filteredPath.filename);
 
-  return { filteredItems, filteredPath };
+  // check if filtered items id was found
+  if (!filteredItems) {
+    logger.error(
+      `⛔ Oh no! Something went wrong: No reference file was found. <br><br> Reason 1: Please provide a properly named file, manually. <br> Reason 2: Referecnce filename doesn't match upload filename. <br><br> • ${filteredPath.filename} <br><br> Please Update the Board manually.`,
+      { additional_info: boardId[0].id }
+    );
+
+    return;
+  }
+
+  return { filteredItems, filteredPath, boardId: boardId[0].id };
 }
--- api/src/utils/handleUpload.ts
+++ api/src/utils/handleUpload.ts
@@ -1,21 +1,44 @@
 /* eslint-disable no-await-in-loop */
+import { LOG_CHANGE_PATH } from "../constants";
+import { getAllBoards } from "../queries/board";
 import { fileUpload } from "../resolvers/fileUpload";
-import { getChangedFiles } from "./getChangedFiles";
 import { getUploadDir } from "./getUploadDir";
+import { readLogs } from "./logReader";
+import { logger } from "./logger";
+import { sendNotificationReport } from "./notificationReport";
 
 // file upload handler
 export async function handleUpload(): Promise<void> {
   // get upload position for file
-  const files = await getChangedFiles();
+  const files = await readLogs(LOG_CHANGE_PATH);
+
   if (files) {
+    // get all boards
+    const boards = await getAllBoards();
+    // console.log(boards);
+
+    // when boards is undefined -> throw ERROR
+    if (!boards) {
+      logger.error(
+        `⛔ Oh no! Something went wrong: No Boards returned. <br><br> Please check API credentials.`
+      );
+
+      return;
+    }
+
     for (const f of files) {
-      console.log(f);
-      const dir = await getUploadDir(f.path);
+      const dir = await getUploadDir(f.msg, boards);
 
       if (dir) {
         // upload file to monday
-        await fileUpload(f.path, dir.filteredItems.id, dir.filteredPath);
+        await fileUpload(f.msg, dir);
       }
     }
+
+    // send notifications
+    await sendNotificationReport();
   }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+handleUpload();
