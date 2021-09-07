aa198dc839e7b0542273ddc11451784aa1dde6f7
paulbuechner <paul-buechner@outlook.de>
2021-09-07 12:10:28 +0200

    feat(api): add `pm2` for process monitoring

---
+++ api/pm2.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name   : "API",
+    script : "./dist/index.js",
+    watch_options : {"usePolling": true},
+    env_production: {
+      NODE_ENV: "production"
+    },
+    env_development: {
+      NODE_ENV: "development",
+    },
+    cron_restart: "1 0 * * *",
+    restart_delay: 4000,
+    error_file: "./logs/pm2/err.log",
+    out_file: "./logs/pm2/out.log",
+    pid_file: "./logs/pm2/API-pm_id.pid"
+  }]
+}
